5408ec4e73d1cd261f73cf6f1f517ff60afb2908
Omar Darwesh <46282255+IamOmarDarwesh@users.noreply.github.com>
2025-09-18 00:16:53 +0300

    initial commit

---
+++ models/staging/sales_stg.sql
@@ -0,0 +1,12 @@
+{{ config(materialized='view') }}
+
+SELECT
+    order_id,
+    product_name,
+    amount,
+    country,
+    order_date,
+    {{ dynamic_partition('order_date', 'MONTH') }}  -- Dynamic partitioning
+FROM `sales-analytics-472318.sales_dataset.raw_sales`
+
+
